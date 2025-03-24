088ea5c904a99ea391cb78340e1224a30a262cff
Arslan Nawaz <arslannawaz293@gmail.com>
2025-03-24 23:52:56 +0500

    Added Database File

---
+++ Database File.sql
@@ -0,0 +1,14 @@
+create database scd_lab8;
+
+create database PrototypeDB;
+
+use PrototypeDB;
+
+INSERT INTO products(name, category, price, color) 
+VALUES ('Lenovo T480', 'Laptop', 65000, 'Black');
+
+select * from products;
+
+create database SingletonDB;
+
+use SingletonDB;
